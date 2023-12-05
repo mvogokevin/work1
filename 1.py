5a301a99cbe685f96a3cf26970517aa4405b8451
mvogokevin <152039594+mvogokevin@users.noreply.github.com>
2023-12-05 09:56:21 +0100

    Python1

---
+++ 1.py
@@ -0,0 +1,14 @@
+# Input string
+input_string = "Hello, World! Hello, Python Prof Mr.Azombo"
+
+# Character to be replaced
+old_char = 'o'
+
+# New character
+new_char = 'u'
+
+# Replacing the character
+output_string = input_string.replace(old_char, new_char)
+
+# Displaying the updated string
+print(output_string)
